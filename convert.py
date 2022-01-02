3166c7d6a2c66fd7179e7522f5a086959a38169e
Jacques Pierre <pierreandassoc.gmail>
2022-01-02 04:20:14 -0700

    worked in provided python starter files

--- convert.py
+++ convert.py
@@ -16,11 +16,30 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    # check validity of arguments
+    if unit_in != "c" and unit_in != "f":
+        return f"Invalid unit {unit_in}."
+    if unit_out != "c" and unit_out != "f":
+        return f"Invalid unit {unit_out}."
+    if type(temp) is not int and type(temp) is not float:
+        return "Invalid temp type (must be int or float)."
 
+    # if unit_in = unit_out, return temp
+    if unit_in == unit_out:
+        return temp
+
+    # if unit_in = "c", use formula: F = 1.8C + 32
+    # else use formula: C = (F - 32) / 1.8
+    if unit_in == "c":
+        F = 1.8 * temp + 32
+        return F
+    C = (temp - 32) / 1.8
+    return C
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
+print("c", "f", "zero", convert_temp("c", "f", "zero"), "should be Invalid temp type (must be int or float).")
 
